6e09330717b7d11f37f4787507ec5c4cb929e188
unknown <kahle.julian@gmail.com>
2022-03-11 12:30:54 +0900

    * excel: only create output for non-existant files (and cut off sub processors which do not create output)

--- processing/excelOutputter/DataProcessor.go
+++ processing/excelOutputter/DataProcessor.go
@@ -7,6 +7,7 @@ import (
 	"returntypes-langserver/common/dataformat/excel"
 	"returntypes-langserver/common/debug/errors"
 	"returntypes-langserver/common/debug/log"
+	"returntypes-langserver/common/utils"
 	"returntypes-langserver/services/predictor"
 	"strings"
 )
@@ -32,32 +33,60 @@ func NewOutputChannel() *OutputChannel {
 }
 
 func NewDatasetProcessor(targetSet configuration.ExcelSet, path string) DatasetProcessor {
+	return newDatasetProcessorInternal(targetSet, path, false)
+}
+
+func newDatasetProcessorInternal(targetSet configuration.ExcelSet, path string, needAllSubprocessors bool) DatasetProcessor {
 	processor := DatasetProcessor{
-		targetSet:         targetSet,
-		subsetProcessors:  make([]DatasetProcessor, 0, len(targetSet.Subsets)),
-		complementChannel: NewOutputChannel(),
-		outputChannel:     NewOutputChannel(),
+		targetSet:        targetSet,
+		subsetProcessors: make([]DatasetProcessor, 0, len(targetSet.Subsets)),
 	}
 	processor.createOutputStreams(path)
 	subdir := filepath.Join(path, targetSet.Name)
+	if !needAllSubprocessors {
+		needAllSubprocessors = processor.complementChannel != nil
+	}
 	for _, subset := range targetSet.Subsets {
-		processor.subsetProcessors = append(processor.subsetProcessors, NewDatasetProcessor(subset, subdir))
+		subsetProcessor := newDatasetProcessorInternal(subset, subdir, needAllSubprocessors)
+		if !needAllSubprocessors && !subsetProcessor.hasOutput() {
+			// filter out subset processors which do not create output so they don't take up resources
+			continue
+		}
+		processor.subsetProcessors = append(processor.subsetProcessors, subsetProcessor)
 	}
 	return processor
 }
 
+func (p *DatasetProcessor) hasOutput() bool {
+	for i := range p.subsetProcessors {
+		if p.subsetProcessors[i].hasOutput() {
+			return true
+		}
+	}
+	return p.outputChannel != nil || p.complementChannel != nil
+}
+
 func (p *DatasetProcessor) createOutputStreams(path string) errors.Error {
 	if !p.targetSet.NoOutput {
-		p.createOutputStream(filepath.Join(path, p.targetSet.Name), p.outputChannel)
+		p.outputChannel = NewOutputChannel()
+		if ok := p.createOutputStream(filepath.Join(path, p.targetSet.Name), p.outputChannel); !ok {
+			p.outputChannel = nil
+		}
 	}
-	if len(p.targetSet.ComplementFilename) > 0 {
-		p.createOutputStream(filepath.Join(path, p.targetSet.ComplementFilename), p.complementChannel)
+	if p.targetSet.ComplementFilename != "" {
+		p.complementChannel = NewOutputChannel()
+		if ok := p.createOutputStream(filepath.Join(path, p.targetSet.ComplementFilename), p.complementChannel); !ok {
+			p.complementChannel = nil
+		}
 	}
 	return nil
 }
 
-func (p *DatasetProcessor) createOutputStream(path string, channel *OutputChannel) {
+func (p *DatasetProcessor) createOutputStream(path string, channel *OutputChannel) bool {
 	xlsxFilePath := path + ".xlsx"
+	if utils.FileExists(xlsxFilePath) {
+		return false
+	}
 	go func() {
 		err := excel.Stream().
 			FromChannel(channel.Output).
@@ -68,10 +97,11 @@ func (p *DatasetProcessor) createOutputStream(path string, channel *OutputChanne
 		log.Info("Saved excel file to: %s\n", xlsxFilePath)
 		channel.Errors <- err
 	}()
+	return true
 }
 
 func (p *DatasetProcessor) process(method csv.Method) {
-	if !p.targetSet.NoOutput {
+	if p.outputChannel != nil {
 		p.outputChannel.Output <- method.ToRecord()
 	}
 	acceptedBySubsetProcessor := false
@@ -81,7 +111,7 @@ func (p *DatasetProcessor) process(method csv.Method) {
 			acceptedBySubsetProcessor = true
 		}
 	}
-	if !acceptedBySubsetProcessor && p.targetSet.ComplementFilename != "" {
+	if !acceptedBySubsetProcessor && p.complementChannel != nil {
 		p.complementChannel.Output <- method.ToRecord()
 	}
 }
@@ -92,11 +122,11 @@ func (p *DatasetProcessor) accepts(method csv.Method) bool {
 }
 
 func (p *DatasetProcessor) close() {
-	if !p.targetSet.NoOutput {
+	if p.outputChannel != nil {
 		close(p.outputChannel.Output)
 		p.checkErrors(p.outputChannel)
 	}
-	if len(p.targetSet.ComplementFilename) > 0 {
+	if p.complementChannel != nil {
 		close(p.complementChannel.Output)
 		p.checkErrors(p.complementChannel)
 	}
